fd79c2eea9c2e666e91c0fa5a65aab2ddeaa122e
wayne <waynekim07@gmail.com>
2025-05-18 01:18:22 +0300

    Added a script file and the document variables

---
+++ script.js
@@ -0,0 +1,38 @@
+const documentVariables = {
+    btns:{
+        addCalorieInpBtn:document.querySelector("#addCalorieInpBtn"),
+        addFoodInpBtn:document.querySelector("#addFoodInpBtn"),
+        addExerciseInpBtn:document.querySelector("#addExerciseInpBtn"),
+        deleteCalorieInpBtn:document.querySelector("#deleteCalorieInpBtn"),
+        deleteFoodInpBtn:document.querySelector("#deleteFoodInpBtn"),
+        deleteExerciseInpBtn:document.querySelector("#deleteExerciseInpBtn"),
+        addFoodBtn:document.querySelector("#addFoodBtn"),
+        
+    },
+    inps:{
+        addCalorieInp:document.querySelectorAll(".addCalorieInp"),
+        addFoodInp:document.querySelectorAll(".addFoodInp"),
+        addExerciseInp:document.querySelectorAll(".addExerciseInp"),
+        deleteCalorieInp:document.querySelectorAll(".deleteCalorieInp"),
+        deleteFoodInp:document.querySelectorAll(".deleteFoodInp"),
+        deleteExerciseInp:document.querySelectorAll(".deleteExerciseInp"),
+        nameInp:document.querySelectorAll(".nameInp"),
+        ageInp:document.querySelectorAll(".ageInp"),
+        heightInp:document.querySelectorAll(".heightInp"),
+        weightInp:document.querySelectorAll(".weightInp"),
+        heightMetricsInp:document.querySelectorAll(".heightMetricsInp"),
+        weightMetricsInp:document.querySelectorAll(".weightMetricsInp"),
+    },
+    sections:{
+        foodInpSection:document.querySelector(".foodInpSection"),
+        calorieInpSection:document.querySelector(".calorieInpSection"),
+        exerciseInpSection:document.querySelector(".exerciseInpSection"),
+        surplusSection:document.querySelectorAll(".surplusSection"),
+        deficeitSection:document.querySelectorAll(".deficeitSection"),
+        currentWeightSection:document.querySelectorAll(".currentWeightSection"),
+        startWeightSection:document.querySelectorAll(".startWeightSection"),
+        filterdSurplusSection:document.querySelectorAll(".filterdSurplusSection"),
+        filterdDeficeitSection:document.querySelectorAll(".filterdDeficeitSection"),
+    }
+}
+
